5fbc4c0b8ee227c114871ef24738ed11072e75bd
stepanusjanu19 <kitoyu22@gmail.com>
2022-10-12 16:40:48 +0700

    project klinik

--- app/Views/dashboard/home.php
+++ app/Views/dashboard/home.php
@@ -12,6 +12,7 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <img src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" style="width:150px;height:150px">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
--- app/Views/login.php
+++ app/Views/login.php
@@ -7,7 +7,7 @@
     <title>Login Page</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?= base_url() ?>/style.css">
+    <link rel="stylesheet" href="<?= base_url('./style.css') ?>">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
